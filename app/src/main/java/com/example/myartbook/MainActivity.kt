98edb904fa0d12ebf5a297e6bb596fe91008c68e
sharnoor404 <sharnoor404@gmail.com>
2020-04-23 17:13:53 +0530

    initial commit

---
+++ app/src/main/java/com/example/myartbook/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myartbook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater=menuInflater
+        menuInflater.inflate(R.menu.add_art,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item?.itemId==R.id.add_art){
+            val intent=Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
