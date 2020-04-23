ef57e316ae6aab6af7eda0cb2fd43d094a0b83e3
sharnoor404 <sharnoor404@gmail.com>
2020-04-24 00:01:15 +0530

    retrieving data functionality

--- app/src/main/java/com/example/myartbook/MainActivity.kt
+++ app/src/main/java/com/example/myartbook/MainActivity.kt
@@ -1,16 +1,52 @@
 package com.example.myartbook
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val artNameArray=ArrayList<String>()
+        val artImageArray=ArrayList<Bitmap>()
+
+        val arrayAdapter=ArrayAdapter(this,android.R.layout.simple_list_item_1,artNameArray)
+        listView.adapter=arrayAdapter
+
+        try {
+            val database=this.openOrCreateDatabase("Arts", Context.MODE_PRIVATE,null)
+            database.execSQL("CREATE TABLE IF NOT EXISTS arts(name VARCHAR,image BLOB)")
+
+            val cursor=database.rawQuery("SELECT * FROM arts",null)
+            val nameIndex=cursor.getColumnIndex("name")
+            val imageIndex=cursor.getColumnIndex("image")
+
+            cursor.moveToFirst()
+            while(cursor!=null){
+                artNameArray.add(cursor.getString(nameIndex))
+                val byteArray=cursor.getBlob(imageIndex)
+                val image=BitmapFactory.decodeByteArray(byteArray,0,byteArray.size)
+                artImageArray.add(image)
+                cursor.moveToNext()
+
+            }
+            cursor?.close()
+
+
+        }catch(e:Exception){
+            e.printStackTrace()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
