24bae64260209be41d422bdd489a8e70fe93cc9a
sharnoor404 <sharnoor404@gmail.com>
2020-04-23 19:54:55 +0530

    permissions

--- app/src/main/java/com/example/myartbook/Main2Activity.kt
+++ app/src/main/java/com/example/myartbook/Main2Activity.kt
@@ -1,12 +1,32 @@
 package com.example.myartbook
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import java.util.jar.Manifest
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+
+    fun select(view:View){
+        if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),2)
+        }
+        else{
+            val intent=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent,1)
+
+        }
+    }
+
+    fun save(view: View){
+
     }
 }
